dce099babf078461ded17337ee2da4609daa0f3f
Jay Parry <jayparry@worknest.com>
2025-09-23 14:57:06 +0100

    Bitlocker Pin set script - pre tested

--- Detecting Bitlocker/detect_bitlocker.ps1
+++ Detecting Bitlocker/detect_bitlocker.ps1
@@ -15,6 +15,10 @@ if ($KeyProtector -contains 'TpmPin' -or $KeyProtector -contains 'TPMAndPIN') {
 
 else {
     Write-Output "No Bitlocker Pin Found"
+    Write-Output "Generating bitlocker pin"
+    $Pin =( Get-Random -Minimum 0 -Maximum 999999 ).ToString(‘000000’)
+    Enable-BitLocker -MountPoint "C:" -EncryptionMethod Aes256 -Pin $Pin -TPMandPinProtector -UsedSpaceOnly
     exit 1
-     
+    Write-Output "Bitlocker Pin Set to $Pin"
+    
      }
