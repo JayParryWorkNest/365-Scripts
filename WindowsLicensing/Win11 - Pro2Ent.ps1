4753c00cbfae93cd81de68ac16776bae07a360c8
Jay Parry <jayparry@worknest.com>
2025-10-01 16:30:37 +0100

    Renamed Pro2Ent script

--- WindowsLicensing/Win11 - Pro2Ent.ps1
+++ WindowsLicensing/Win11 - Pro2Ent.ps1
@@ -1,9 +1,9 @@
 # Use the KMS key to upgrade to Windows Enterprise
-#slmgr.vbs //b /ipk XGVPP-NMH47-7TTHJ-W3FW7-8HV2C
+slmgr.vbs //b /ipk XGVPP-NMH47-7TTHJ-W3FW7-8HV2C
 
 # Reapply the original subscription license key
-#$subscriptionlicense = (Get-WmiObject SoftwareLicensingService).OA3xOriginalProductKey
-#slmgr.vbs /ipk $subscriptionlicense
+$subscriptionlicense = (Get-WmiObject SoftwareLicensingService).OA3xOriginalProductKey
+slmgr.vbs /ipk $subscriptionlicense
 
 
 # Force upgrade to Windows Enterprise using KMS Client Setup Key
